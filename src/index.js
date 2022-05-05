a7ab23702a7b0ba81831f7d6ed7d267f1e039290
shlommi <shlommi@gmail.com>
2022-05-05 21:26:47 +0300

    remove console.log

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let loadingNumber = 0;
+let countingInterval = setInterval(showBlurring, 30);
+
+function showBlurring() {
+  loadingNumber++;
+  if (loadingNumber > 99) {
+    clearInterval(countingInterval);
+  }
+  loadText.innerText = `${loadingNumber}%`;
+  loadText.style.opacity = `${scale(loadingNumber, 0, 100, 1, 0)}`;
+  bg.style.filter = `blur(${scale(loadingNumber, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
